2f4d403dc2f4c1d21893fc661e25f7db8bf6ae16
Tim Chan <macrotim@gmail.com>
2015-11-05 06:28:04 -0800

    Added demo of functools.partial.

---
+++ functools/partial.py
@@ -0,0 +1,22 @@
+"""Raymond Hettinger recommends using functools.partial to rewrite
+   a "while True" into a for-loop. https://www.youtube.com/watch?v=OSGv2VnC0go"""
+import random
+from functools import  partial
+
+def next_num():
+    return random.randint(0,10)
+
+# DO.
+for i in iter(partial(next_num), 0):
+    print i
+
+"""DON'T.
+
+while True:
+    i = next_num()
+    if i == 0:
+        break
+    print i
+"""
+
+print 'done'
