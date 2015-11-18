f906d6949ac47baaf691e61309d512926c149123
Tim Chan <macrotim@gmail.com>
2015-11-18 10:21:39 -0800

    Tightened up whitespace.

--- boto-s3/s3_locations_test.py
+++ boto-s3/s3_locations_test.py
@@ -15,30 +15,28 @@ from mock import patch
 
 from s3_locations import run_benchmark
 
+B = 'boto-benchmark-unit-test'
+
 class PatcherStartStopTest(unittest.TestCase):
 
     def setUp(self):
         patcher = patch('s3_locations.S3Connection')
         self.s3_connection_mock = patcher.start()
-        self.s3_connection_mock.return_value = self.s3_connection_mock
+        self.s3_connection_mock.return_value = self.s3_connection_mock  # guess what happens without this?
         self.addCleanup(patcher.stop)
 
     def test_pass(self):
-        run_benchmark('boto-benchmark-unit-test', [], num_files=0)
+        run_benchmark(B, [], num_files=0)
         self.s3_connection_mock.assert_not_called()
 
     def test_create_bucket(self):
-        run_benchmark('boto-benchmark-unit-test', (Location.USWest,), num_files=0)
-        self.s3_connection_mock.create_bucket.assert_called_with('boto-benchmark-unit-test-us-west-1', location=Location.USWest)
+        run_benchmark(B, (Location.USWest,), num_files=0)
+        self.s3_connection_mock.create_bucket.assert_called_with(B + '-us-west-1', location=Location.USWest)
 
     def test_get_bucket(self):
-        self.s3_connection_mock.create_bucket.side_effect = (
-            S3CreateError(1, 2, 3), # Error the first time.
-            None)                   # Do nothing the second time.
-
-        run_benchmark('boto-benchmark-unit-test', (Location.USWest,), num_files=0)
-
-        b = 'boto-benchmark-unit-test-us-west-1'
+        b = B + '-us-west-1'
+        self.s3_connection_mock.create_bucket.side_effect = (S3CreateError(1, 2, 3), None)
+        run_benchmark(B, (Location.USWest,), num_files=0)
         self.s3_connection_mock.get_bucket.assert_called_with(b)
         self.s3_connection_mock.delete_bucket.assert_called_with(b)
         self.s3_connection_mock.create_bucket.assert_called_with(b, location=Location.USWest)
